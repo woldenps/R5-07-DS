5535c9937afa6c47263d7e2a502aaf870b33b839
woldenps <166333957+woldenps@users.noreply.github.com>
2025-10-08 16:11:26 +0200

    Refactor Q6.java to simplify functionality
    Removed input validation and loop for printing 'x'. Added a test print statement.

--- Q6.java
+++ Q6.java
@@ -1,17 +1,5 @@
 public class Q6 {
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage : java Q6 <N>");
-            return;
-        }
-
-        int N = Integer.parseInt(args[0]);
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("x ");
-            }
-            System.out.println();
-        }
+        System.out.println("test");
     }
 }
