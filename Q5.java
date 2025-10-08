a7356edae21badad04f6471dc28fc0f78f496583
woldenps <166333957+woldenps@users.noreply.github.com>
2025-10-08 16:05:19 +0200

    Fix #2

--- Q5.java
+++ Q5.java
@@ -7,9 +7,9 @@ public class Q5 {
 
         int N = Integer.parseInt(args[0]);
 
-        for (int i = N; i >= 1; i--) {
+        for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
+                System.out.print("x ");
             }
             System.out.println();
         }
