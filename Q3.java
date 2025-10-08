63d5cf78cf8cafb053ae235bee019b09d4aab8ed
woldenps <166333957+woldenps@users.noreply.github.com>
2025-10-08 16:13:22 +0200

    Fix formatting and add braces for clarity

--- Q3.java
+++ Q3.java
@@ -1,13 +1,15 @@
 public class Q3 {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length != 1) 
+        {
             System.out.println("Usage : java Q3 <N>");
             return;
         }
 
         int N = Integer.parseInt(args[0]);
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) 
+        {
             System.out.print(i);
             if (i < N) System.out.print(" ");
         }
