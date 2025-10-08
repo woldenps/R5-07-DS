860e3d6c46edbe84bf186dd4b24df12f14a43fbe
woldenps <166333957+woldenps@users.noreply.github.com>
2025-10-08 16:14:08 +0200

    Fix formatting of main method in Q4.java

--- Q4.java
+++ Q4.java
@@ -1,5 +1,6 @@
 public class Q4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         if (args.length != 1) 
         {
             System.out.println("Usage : java Q4 <N>");
