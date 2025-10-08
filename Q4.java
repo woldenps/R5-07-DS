e82482db169e6a94dff555a7b8de6399096a55c6
woldenps <166333957+woldenps@users.noreply.github.com>
2025-10-08 16:13:41 +0200

    Fix formatting and indentation in Q4.java

--- Q4.java
+++ Q4.java
@@ -1,14 +1,17 @@
 public class Q4 {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length != 1) 
+        {
             System.out.println("Usage : java Q4 <N>");
             return;
         }
 
         int N = Integer.parseInt(args[0]);
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; i <= N; i++) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
                 System.out.print(j + " ");
             }
             System.out.println();
