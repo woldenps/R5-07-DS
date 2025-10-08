4ae550fd35d6e698581e4e94753041bb19522f0a
woldenps <166333957+woldenps@users.noreply.github.com>
2025-10-08 16:14:41 +0200

    Reformat Q6.java for consistent brace style

--- Q6.java
+++ Q6.java
@@ -1,5 +1,7 @@
-public class Q6 {
-    public static void main(String[] args) {
+public class Q6 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("test");
     }
 }
